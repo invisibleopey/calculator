3289656d82924b9b531e6b5106a9da8d4c365469
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-05-16 23:39:14 +0100

    Cleaned up the code

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
+const display = document.querySelector("#display");
+const digitBtns = document.querySelectorAll(".digits");
+const operatorBtns = document.querySelectorAll(".operators");
+const equalSignBtn = document.querySelector("#equal");
+const clearBtn = document.querySelector("#clear");
+const deleteBtn = document.querySelector("#delete");
+const decimalBtn = document.querySelector("#decimal");
+
 let operand1 = "";
 let operand2 = "";
 let operator = null;
+let displayValue = display.textContent;
+// Event listeners
+digitBtns.forEach(button => button.addEventListener("click", populateDisplay));
+operatorBtns.forEach(button => button.addEventListener("click", selectOperator));
+equalSignBtn.addEventListener("click", findResult);
+clearBtn.addEventListener("click", clear);
+deleteBtn.addEventListener("click", deleteNumber);
+decimalBtn.addEventListener("click", insertDecimal);
 
+// Functions for handling the mathematical operations
 function add (a, b) {
     return Number(a) + Number(b);
 }
-
 function subtract (a, b) {
     return Number(a) - Number(b);
 }
-
 function multiply(a, b) {
     return Number(a) * Number(b);
 }
-
 function divide(a,b) {
     return Number(a) / Number(b);
 }
-
 function operate (operand1, operator, operand2) {
     if (operator === "+") return add(operand1,operand2);
     if (operator === "-") return subtract(operand1,operand2);
     if (operator === "*") return multiply(operand1,operand2);
     if (operator === "/") return divide(operand1,operand2);
 }
-// Displaying Digits pressed
-
-const display = document.querySelector("#display");
-const digitBtns = document.querySelectorAll(".digits");
-
-let displayValue = display.textContent;
-digitBtns.forEach(button => button.addEventListener("click", populateDisplay));
 
 function populateDisplay (e) {
     if (displayValue === "0" || displayValue === operand1) {
@@ -38,30 +44,16 @@ function populateDisplay (e) {
     } else {
         displayValue += e.srcElement.innerText;
     }
-    // console.log(e.srcElement.innerText);
-    // displayValue = display.textContent
     display.textContent = displayValue;
 }
 
-// Making the calculator work 
-const operatorBtns = document.querySelectorAll(".operators");
-operatorBtns.forEach(button => button.addEventListener("click", operationFunc));
-
-function operationFunc (e) {
-    // console.log(e.srcElement.innerText);
+function selectOperator (e) {
     if (operator !== null ) {
         findResult();
     }
         operand1 = displayValue;
         operator = e.srcElement.innerText;
-
-    // }
-    // num1 = displayValue;
-    // display.textContent = "0";
 }
-
-const equalSign = document.querySelector("#equal");
-equalSign.addEventListener("click", findResult);
 
 function findResult () {
     if (operator === null) {
@@ -78,8 +70,6 @@ function findResult () {
     operator = null;
 }
 
-const clearBtn = document.querySelector("#clear");
-clearBtn.addEventListener("click", clear);
 function clear() {
     displayValue = "0";
     display.textContent = "0";
@@ -88,16 +78,12 @@ function clear() {
     operator = null;
 }
 
-const deleteBtn = document.querySelector("#delete");
-deleteBtn.addEventListener("click", deleteNumber);
 function deleteNumber() {
     display.textContent = display.textContent.toString().slice(0, -1);
     // Set the new displayValue to the new text in case want to use it for operation
     displayValue = display.textContent;
+    operator = null;
 }
-
-const decimalBtn = document.querySelector("#decimal");
-decimalBtn.addEventListener("click", insertDecimal);
 
 function insertDecimal (e) {
     if (display.textContent.includes(".")) return
