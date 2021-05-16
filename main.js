92283034a5564c37684e00a3041f9fd8e91acd64
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-05-16 15:48:25 +0100

    Added populateDisplay function to showed the digit buttons clicked

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let num1, operator, num2;
+
 function add (a, b) {
     return a + b;
 }
@@ -20,3 +22,17 @@ function operate (num1, operator, num2) {
     if (operator === "*") return multiply(num1,num2);
     if (operator === "/") return divide(num1,num2);
 }
+// Displaying Digits pressed
+
+const display = document.querySelector("#display");
+let displayValue = "";
+const digitBtns = document.querySelectorAll(".digits");
+digitBtns.forEach(button => button.addEventListener("click", populateDisplay));
+
+function populateDisplay (e) {
+    // console.log(e.srcElement.innerText);
+    display.textContent += e.srcElement.innerText;
+    displayValue = display.textContent
+}
+
+// Making the calculator work 
